b8c1d786273c0dc87d46a1a8407f2f6a1c26d78e
vojtech.vit <vit.vo@email.cz>
2021-09-15 13:27:10 +0200

    ahoj

--- src/cz/spsmb/b3/Application.java
+++ src/cz/spsmb/b3/Application.java
@@ -1,8 +1,20 @@
 package cz.spsmb.b3;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadej hodnotu");
+        int number = sc.nextInt();
+        int counter = 0;
+        int rest = 0;
+        int [] array = {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+        for (int i = 0; i < array.length; i++) {
+            counter = number/array[i];
+            System.out.println(number + " = " + counter + " * " + array[i]);
+            number = number%array[i];
+        }
 
     }
 }
